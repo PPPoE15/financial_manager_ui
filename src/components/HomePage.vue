41c013963d8aafd8932586521f56ca1e2cc6fde8
Артем Викторов <aviktorov@astralinux.ru>
2025-10-23 11:52:30 +0300

    added home page with add transaction button

---
+++ src/components/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <button 
+      @click="$router.push('/add-transaction')"
+      class="bg-blue-500 text-white px-4 py-2 rounded"
+    >
+      Добавить транзакцию
+    </button>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AddTransactionView from '../views/AddTransactionView.vue'
+import HomePage from '@/components/HomePage.vue'
 
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage
+  },
   {
     path: '/add-transaction',
     name: 'AddTransaction',
